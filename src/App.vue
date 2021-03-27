54f3d5d2e6374df3c58291a1ff2147d1cedd0635
Isaac Azevedo <isaacslima@yahoo.com.br>
2021-03-27 01:17:10 -0300

    add rotas

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,30 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-main>
+      <router-view />
+    </v-main>
+    <v-bottom-navigation v-model="value">
+      <v-btn value="recent" to="/produtos">
+        <span>Produtos</span>
+        <v-icon>mdi-toy-brick</v-icon>
+      </v-btn>
+      <v-btn value="favorites" to="/clientes">
+        <span>Clientes</span>
+        <v-icon>mdi-account</v-icon>
+      </v-btn>
+      <v-btn value="nearby" to="/">
+        <span>Home</span>
+        <v-icon>mdi-home</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
+  </v-app>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<script>
+export default {
+  name: 'App',
+  data: () => ({
+    drawer: false,
+    group: null,
+  }),
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Clientes from '../views/Clientes.vue'
+import Produtos from '../views/Produtos.vue'
 
 Vue.use(VueRouter)
 
@@ -11,13 +13,23 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/clientes',
+    name: 'Clientes',
+    component: Clientes
+  },
+  {
+    path: '/produtos',
+    name: 'Produtos',
+    component: Produtos
+  },
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
 ]
 
 const router = new VueRouter({
