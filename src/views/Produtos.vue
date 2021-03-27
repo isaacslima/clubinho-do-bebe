1ed4811830da246c06c429d6e5b14b14c660381a
Isaac Azevedo <isaacslima@yahoo.com.br>
2021-03-27 02:44:53 -0300

    add lista produtos

--- src/views/Produtos.vue
+++ src/views/Produtos.vue
@@ -1,7 +1,22 @@
 <template>
   <div class="about">
     <h1>Produtos</h1>
-    {{ produtos }}
+    <v-list three-line>
+      <template v-for="(produto, index) in produtos">
+        <v-divider :key="index" inset></v-divider>
+
+        <v-list-item :key="produto.nome">
+          <v-list-item-avatar class="avatar">
+            <img :src="produto.localFoto"/>
+          </v-list-item-avatar>
+
+          <v-list-item-content>
+            <v-list-item-title v-html="produto.nome"></v-list-item-title>
+            <v-list-item-subtitle> Preço <b>R$ {{ produto.preco }}</b></v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </template>
+    </v-list>
   </div>
 </template>
 <script>
@@ -38,3 +53,10 @@ export default {
   },
 };
 </script>
+<style>
+.avatar{
+  height: 60px !important;
+  min-width: 60px !important;
+  width: 60px !important;
+}
+</style>
