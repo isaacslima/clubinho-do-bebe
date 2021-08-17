4b3ee4a59a5bfe92e52e4b4672f04799caad4f21
Isaac Sousa <isaac.azevedo@mrv.com.br>
2021-08-17 00:10:32 -0300

    creating home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,57 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <v-list three-line>
+      <template v-for="(item) in items">
+        <v-list-item
+          :key="item.title"
+          class="menu-home"
+          @click="changePage(item.title)"
+        >
+          <v-list-item-icon>
+            <v-icon color="orange" x-large v-text="item.icon"></v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title v-html="item.title"></v-list-item-title>
+            <v-list-item-subtitle v-html="item.subtitle"></v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </template>
+    </v-list>
   </div>
 </template>
 
+<script>
+  export default {
+    data: () => ({
+      items: [
+        {
+          icon: 'mdi-toy-brick',
+          title: 'Produtos',
+          subtitle: `Lista de produtos, preços e itens disponíveis para aluguel.`,
+        },
+        {
+          icon: 'mdi-archive',
+          title: 'Aluguel',
+          subtitle: `Acompanhamento de itens locados e datas de devolução`,
+        },
+        {
+          icon: 'mdi-account-multiple',
+          title: 'Clientes',
+          subtitle: 'Cadastro e atualização de clientes',
+        },
+        {
+          icon: 'mdi-calendar-multiselect',
+          title: 'Calendário',
+          subtitle: 'Calendário de itens locados, datas de devolução e agendamento',
+        },
+      ],
+    }),
+  }
+</script>
+
+<style>
+.menu-home{
+  border-bottom: 1px solid black;
+}
+</style>
