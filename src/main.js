770434df467c93aa34861d751404d020154f4179
Isaac Sousa <isaacslima@yahoo.com.br>
2021-09-07 01:27:47 -0300

    adding firestore plugin

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import router from './router'
+import { firestorePlugin } from 'vuefire'
 
 Vue.config.productionTip = false
+
+Vue.use(firestorePlugin)
 
 new Vue({
   vuetify,
