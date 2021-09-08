1e5a04484ec6103f330122f2b72235927606f702
Isaac Sousa <isaacslima@yahoo.com.br>
2021-09-08 01:26:39 -0300

    add search for clients

--- src/views/Clientes.vue
+++ src/views/Clientes.vue
@@ -1,30 +1,57 @@
 <template>
-  <div class="about">
-    <h1>Clientes</h1>
-    <v-data-table
-      :headers="headers"
-      :items="clientes"
-      :items-per-page="5"
-      class="elevation-1"
-    ></v-data-table>
-    <div v-for="item in clientes" :key="item.message">
-    </div>
-  </div>
+  <v-container class="clientes">
+    <v-row fixed>
+      <v-col>
+        <h1>Clientes</h1>
+    </v-col>
+    <v-spacer>
+    </v-spacer>
+    <v-col>
+      <v-btn>Novo Cliente</v-btn>
+    </v-col>
+    </v-row>
+    
+    
+    <v-card>
+      <v-card-title>
+        <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Pesquisar"
+        single-line
+        hide-details
+        ></v-text-field>
+        <v-data-table
+          :headers="headers"
+          hide-default-header
+          :items="clientes"
+          :items-per-page="5"
+          class="elevation-1"
+        ></v-data-table>
+      </v-card-title>
+    </v-card>
+  </v-container>
 </template>
 <script>
 import firebase from "../firebase/index";
+import 'firebase/storage';
 const db = firebase.firestore();
 
 export default {
   name: "clientes",
+  firestore: {
+    clientes: db.collection('clientes'),
+  },
   data() {
     return {
+      search: '',
       clientes: [],
       headers: [
           {
             text: 'Nome',
             align: 'start',
             value: 'nome',
+            filterable: true,
           },
           { text: 'Telefone', value: 'telefone' },
           { text: 'CPF', value: 'cpf' },
@@ -33,24 +60,11 @@ export default {
     };
   },
   mounted () {
-    let _clientes = [];
-    var self = this
-    db.collection("clientes")
-    .get()
-    .then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-        _clientes.push({
-          id: doc.id,
-          nome: doc.data().nome,
-          telefone: doc.data().telefone,
-          cpf: doc.data().cpf
-        });
-      });
-      self.clientes = _clientes;
-    })
-    .catch((error) => {
-      console.log("Error getting documents: ", error);
-    });
   },
 };
 </script>
+<style scoped>
+.clientes{
+  margin-top: 30px;
+}
+</style>
