c890032d19a0d9ae33908e8de00f84848163724e
Isaac Sousa <isaacslima@yahoo.com.br>
2021-09-08 01:40:29 -0300

    adding form to client page

--- src/views/Clientes.vue
+++ src/views/Clientes.vue
@@ -7,11 +7,9 @@
     <v-spacer>
     </v-spacer>
     <v-col>
-      <v-btn>Novo Cliente</v-btn>
+      <v-btn @click="adicionarCliente()">Novo Cliente</v-btn>
     </v-col>
     </v-row>
-    
-    
     <v-card>
       <v-card-title>
         <v-text-field
@@ -30,6 +28,46 @@
         ></v-data-table>
       </v-card-title>
     </v-card>
+    <v-dialog v-model="dialog" fullscreen hide-overlay transition="dialog-bottom-transition">
+        <v-card>
+          <v-form ref="form" @submit.prevent="salvar">
+            <v-app-bar dark fixed color="primary" dense elevate-on-scroll>
+              <v-btn icon dark @click="resetForm">
+                <v-icon>mdi-close</v-icon>
+              </v-btn>
+              <v-toolbar-title>{{ acaoCliente }}</v-toolbar-title>
+              <v-spacer></v-spacer>
+              <v-toolbar-items>
+                <v-btn dark text @click="resetForm" type="button">
+                  Cancelar
+                </v-btn>
+                <v-btn dark text type="submit" :disabled="!formIsValid">
+                  Salvar
+                </v-btn>
+              </v-toolbar-items>
+            </v-app-bar>
+            <v-container>
+              <v-row>
+                <v-col cols="12" md="6">
+                  <v-text-field outlined v-model="form.nome" label="Nome" required></v-text-field>
+                </v-col>
+                <v-col cols="12" md="6">
+                  <v-text-field outlined v-model="form.telefone" label="Telefone" required></v-text-field>
+                </v-col>
+                <v-col cols="12" md="6">
+                  <v-text-field outlined v-model="form.cpf" label="CPF" required></v-text-field>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-form>
+        </v-card>
+      </v-dialog>
+    <v-snackbar v-model="snackbar" :color="color">
+      <v-icon dark>
+        {{ icon }}
+      </v-icon>
+      <span>{{ mensagem }}</span>
+    </v-snackbar>
   </v-container>
 </template>
 <script>
@@ -43,6 +81,11 @@ export default {
     clientes: db.collection('clientes'),
   },
   data() {
+    const defaultForm = Object.freeze({
+        nome: '',
+        telefone: '',
+        cpf: '',
+      })
     return {
       search: '',
       clientes: [],
@@ -56,10 +99,39 @@ export default {
           { text: 'Telefone', value: 'telefone' },
           { text: 'CPF', value: 'cpf' },
         ],
-      unsubscribe: null
+      unsubscribe: null,
+      snackbar: false,
+      mensagem: '',
+      color: '',
+      icon: '',
+      dialog: false,
+      form: Object.assign({}, defaultForm),
+      acaoCliente: ''
     };
   },
   mounted () {
+  },
+  methods: {
+    adicionarCliente () {
+
+    },
+    salvar() {
+
+    },
+    resetForm () {
+      this.form = Object.assign({}, this.defaultForm)
+      this.$refs.form.reset()
+      this.dialog = false
+    },
+  },
+  computed: {
+    formIsValid () {
+      return (
+        this.form.nome &&
+        this.form.telefone &&
+        this.form.cpf
+      )
+    },
   },
 };
 </script>
