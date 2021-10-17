ceafd1b7c7ae29f6cd3a583db2ae17deaf5addcd
Isaac Azevedo <isaacslima@yahoo.com.br>
2021-10-16 23:04:11 -0300

    form para lançar aluguel

--- src/views/Aluguel.vue
+++ src/views/Aluguel.vue
@@ -54,6 +54,123 @@
         </v-card>
       </v-container>
     </v-row>
+    <v-row>
+      <v-dialog v-model="dialog" fullscreen hide-overlay transition="dialog-bottom-transition">
+        <v-card>
+          <v-form ref="form" @submit.prevent="salvar">
+            <v-app-bar dark fixed color="primary" dense elevate-on-scroll>
+              <v-btn icon dark @click="resetForm">
+                <v-icon>mdi-close</v-icon>
+              </v-btn>
+              <v-toolbar-title>{{ acaoAluguel }}</v-toolbar-title>
+              <v-spacer></v-spacer>
+              <v-toolbar-items>
+                <v-btn dark text @click="resetForm" type="button">
+                  Cancelar
+                </v-btn>
+                <v-btn dark text type="submit" :disabled="!formIsValid || loading">
+                  Salvar
+                </v-btn>
+              </v-toolbar-items>
+            </v-app-bar>
+            <v-container class="container-add-edit">
+              <v-row>
+                <v-col cols="12" md="6">
+                  <v-select outlined v-model="form.cliente" :items="clientes" label="Cliente" required
+                    item-value="item"
+                  >
+                     <template v-slot:selection="data">
+                        {{ data.item.nome }} - {{ data.item.telefone }}
+                      </template>
+                      <template v-slot:item="data">
+                        {{ data.item.nome }} - {{ data.item.telefone }}
+                      </template>
+                  </v-select>
+                </v-col>
+                <v-col cols="12" md="6">
+                  <v-select 
+                    outlined 
+                    v-model="form.produto" 
+                    :items="produtos" 
+                    label="Produto"
+                    item-value="item"
+                    required
+                  >
+                  <template v-slot:selection="data">
+                    {{ data.item.descricao }}
+                  </template>
+                  <template v-slot:item="data">
+                    {{ data.item.descricao }}
+                  </template>
+                  </v-select>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col cols="12" md="2">
+                  <v-menu
+                    outlined
+                    v-model="menu"
+                    :close-on-content-click="false"
+                    :nudge-right="40"
+                    transition="scale-transition"
+                    offset-y
+                    min-width="auto"
+                  >
+                    <template v-slot:activator="{ on, attrs }">
+                      <v-text-field
+                        outlined
+                        v-model="form.dataAluguel"
+                        label="Data Aluguel"
+                        prepend-icon="mdi-calendar"
+                        readonly
+                        v-bind="attrs"
+                        v-on="on"
+                      ></v-text-field>
+                    </template>
+                    <v-date-picker
+                      v-model="form.dataAluguel"
+                      @input="menu = false"
+                    ></v-date-picker>
+                  </v-menu>
+                </v-col>
+                <v-col cols="12" md="3">
+                  <v-select 
+                      outlined 
+                      v-model="form.precoDia" 
+                      :items="form.produto.precos" 
+                      label="Dias e Preço"
+                      item-value="item"
+                      required
+                      :disabled="form.produto === ''"
+                      >
+                    <template v-slot:selection="data">
+                      {{ data.item.dias }} dias R$ {{ data.item.preco}}
+                    </template>
+                    <template v-slot:item="data">
+                      {{ data.item.dias }} dias R$ {{ data.item.preco}}
+                    </template>
+                  </v-select>
+                </v-col>
+                <v-col cols="6" md="2">
+                  <v-text-field outlined v-model="form.desconto" type="number" label="Desconto" required></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                  {{ form.precoDia || json }}
+                  Total R$ {{ form.precoDia.preco - form.desconto }}
+                </v-col>
+              </v-row>
+
+            </v-container>
+          </v-form>
+        </v-card>
+      </v-dialog>
+    </v-row>
+    <v-snackbar v-model="snackbar" :color="color">
+      <v-icon dark>
+        {{ icon }}
+      </v-icon>
+      <span>{{ mensagem }}</span>
+    </v-snackbar>
   </div>
 </template>
 <script>
@@ -65,23 +182,51 @@ export default {
   name: "aluguel",
   firestore: {
     alugueis: db.collection('aluguel'),
+    produtos: db.collection('produtos'),
+    clientes: db.collection('clientes')
   },
   data() {
     const defaultForm = Object.freeze({
-        nomeProduto: '',
-        nomeClient: ''
+        produto: '',
+        cliente: '',
+        dataAluguel: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+        dataDevolucao: '',
+        precoDia: {
+          dias: 10,
+          preco: 0
+        },
+        desconto: 0
     })
     return {
       acaoAluguel: '',
       alugueis: [],
       form: Object.assign({}, defaultForm),
-      dialog: false
+      dialog: false,
+      snackbar: false,
+      mensagem: '',
+      icon: '',
+      color: '',
+      menu: false
     };
   },
   created() {
     this.$root.$refs.Children = this;
   },
+  computed: {
+    formIsValid () {
+      return (
+        this.form.idProduto &&
+        this.form.idCliente &&
+        this.form.dataAluguel &&
+        this.form.dataDevolucao
+      )
+    },
+  },
   methods: {
+    retornaStringCliente (cliente) {
+      console.log(cliente);
+      return 'teste';
+    },
     resetForm () {
     },
     adicionarItem () {
