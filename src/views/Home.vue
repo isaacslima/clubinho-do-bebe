78c902741230c5c131e55bf49b4edc1f42ccaf8c
Isaac Azevedo <isaacslima@yahoo.com.br>
2021-10-18 23:35:57 -0300

    removendo metodo não utilizado

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,13 +5,11 @@
         <v-list-item
           :key="item.title"
           class="menu-home"
-          @click="changePage(item.title)"
           :to="item.location"
         >
           <v-list-item-icon>
             <v-icon color="orange" x-large v-text="item.icon"></v-icon>
           </v-list-item-icon>
-
           <v-list-item-content>
             <v-list-item-title v-html="item.title"></v-list-item-title>
             <v-list-item-subtitle v-html="item.subtitle"></v-list-item-subtitle>
@@ -24,7 +22,6 @@
 
 <script>
 import items from '../shared/items'
-
   export default {
     data: () => ({
       items: items
