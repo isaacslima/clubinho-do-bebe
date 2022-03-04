4d65f715d42476a71714c832baa32a740beaf7a8
Isaac de Sousa <isaacslima@yahoo.com.br>
2022-03-03 23:25:14 -0300

    adding calendar

--- src/main.js
+++ src/main.js
@@ -3,9 +3,10 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import router from './router'
 import { firestorePlugin } from 'vuefire'
+import Calendar from 'v-calendar/lib/components/calendar.umd'
 
+Vue.component('calendar', Calendar)
 Vue.config.productionTip = false
-
 Vue.use(firestorePlugin)
 
 new Vue({
--- src/views/Calendario.vue
+++ src/views/Calendario.vue
@@ -1,5 +1,67 @@
 <template>
-<div>
-    Calendario
+<div class="calendar">
+    <Calendar is-expanded trim-weeks :rows="2"
+        :attributes="attributes"
+        >
+        </Calendar>
+    <div v-for="(aluguel) in alugueis" :key="aluguel.id">
+        Data Vencimento: {{ new Date(aluguel.dataDevolucao.seconds*1000).toLocaleDateString('pt-BR') }}
+    </div>
 </div>
 </template>
+<script>
+import firebase from "../firebase/index";
+import 'firebase/storage';
+const db = firebase.firestore();
+import Calendar from 'v-calendar/lib/components/calendar.umd'
+
+export default {
+    components: {
+    Calendar
+  },
+  name: "aluguel",
+  firestore: {
+    alugueis: db.collection('aluguel'),
+  },
+  data() {
+    const todos = [
+      {
+        description: 'Take Noah to basketball practice.',
+        isComplete: false,
+        dates: { weekdays: 6 }, // Every Friday
+        color: 'red',
+      },
+    ];
+    return {
+        selectedDate: null,
+        alugueis: [],
+        todos
+    };
+  },
+  computed: {
+    attributes() {
+        var teste = 
+                    [
+                        ...this.todos.map(todo => ({
+                        dates: todo.dates,
+                        dot: {
+                            color: todo.color,
+                            class: todo.isComplete ? 'opacity-75' : '',
+                        },
+                        popover: {
+                            label: todo.description,
+                        },
+                        customData: todo,
+                        })),
+                    ];
+        console.log(teste);
+      return teste;
+    },
+  },
+};
+</script>
+<style scoped>
+.calendar{
+    margin-top: 50px;
+}
+</style>
